3a185a6a08cd78beee50a655d45422f89426c47d
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-08-05 17:32:10 -0400

    probando cambio de marca de tiempo

--- src/main/java/com/galileo/cu/servicioimportador/ServicioImportadorApplication.java
+++ src/main/java/com/galileo/cu/servicioimportador/ServicioImportadorApplication.java
@@ -20,7 +20,7 @@ public class ServicioImportadorApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("**************************************");
-        System.out.println("Importador V-2024-08-5 16:57");
+        System.out.println("Importador V-2024-08-5 17:31");
     }
 
 }
