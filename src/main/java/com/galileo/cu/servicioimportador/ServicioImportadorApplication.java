36431293d4228e9b7121a746ad6f2c970cfc1408
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-17 12:26:33 -0400

    4- Fix estructure the folder for deploy

--- src/main/java/com/galileo/cu/servicioimportador/ServicioImportadorApplication.java
+++ src/main/java/com/galileo/cu/servicioimportador/ServicioImportadorApplication.java
@@ -20,7 +20,7 @@ public class ServicioImportadorApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("**************************************");
-        System.out.println("Importador V1.1-2024-10-17 11:17");
+        System.out.println("Importador V1.1-2024-10-17 12:26");
     }
 
 }
