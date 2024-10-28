16e257b794c87e4756c20335b20eb83f0d7502b7
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-10-28 14:42:59 -0400

    VERSION 1.1.1 AFTER FIXED BUGS V 1.1.0 AFTER QA OK

--- src/main/java/com/galileo/cu/servicioimportador/ServicioImportadorApplication.java
+++ src/main/java/com/galileo/cu/servicioimportador/ServicioImportadorApplication.java
@@ -20,7 +20,7 @@ public class ServicioImportadorApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("**************************************");
-        System.out.println("Importador V1.1-2024-10-25 04:42");
+        System.out.println("Importador V-1.1.1 2024-10-28 14:42");
     }
 
 }
