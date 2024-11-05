790827a84bb2f035bbda983c130694632f035383
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-11-05 15:51:14 -0500

    mapAddress Recompilando

--- src/main/java/com/galileo/cu/servicioimportador/ServicioImportadorApplication.java
+++ src/main/java/com/galileo/cu/servicioimportador/ServicioImportadorApplication.java
@@ -20,7 +20,7 @@ public class ServicioImportadorApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("**************************************");
-        System.out.println("Importador V-1.1.1 2024-10-30 16:11");
+        System.out.println("Importador V-1.1.1 2024-11-05 15:50");
     }
 
 }
