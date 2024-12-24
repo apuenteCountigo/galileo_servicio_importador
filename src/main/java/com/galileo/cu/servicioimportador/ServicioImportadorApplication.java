aed9a932cdf24d3fb204e69b9a5333c9cba4fff7
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-12-24 13:41:08 -0500

    V1.1.4

--- src/main/java/com/galileo/cu/servicioimportador/ServicioImportadorApplication.java
+++ src/main/java/com/galileo/cu/servicioimportador/ServicioImportadorApplication.java
@@ -20,7 +20,7 @@ public class ServicioImportadorApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         System.out.println("**************************************");
-        System.out.println("Importador V-1.1.2 2024-11-05 16:13");
+        System.out.println("Importador V-1.1.4 24-12-24 13:39");
     }
 
 }
